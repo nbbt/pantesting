bf6cb439fcb0944395b7b9b78793511ed5ac41c9
KimiNewt <dorgreen1@gmail.com>
2014-02-07 00:51:26 +0200

    whatever

--- src/pantesting/static/js/hosts.js
+++ src/pantesting/static/js/hosts.js
@@ -10,9 +10,18 @@ angular.module('pantestingApp').controller('Hosts',
 angular.module('pantestingApp').controller('HostDetails',
   function($scope, $routeParams, $rootScope) {
       $scope.host = $scope.getHost($routeParams.hostId);
+      $scope.bounties = [];
 
       $scope.isUserHostOwner = function() {
           return $rootScope.currentUser && $scope.host.user.name == $rootScope.currentUser.name;
+      }
+      $scope.getBounties = function() {
+          $http.get('/get_bounties/' + $scope.host.id).success(function(data) {
+              $scope.bounties = data.bounties;
+          })
+      }
+      $scope.submitExploit = function(exploitId) {
+
       }
   });
 
