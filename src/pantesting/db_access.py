79bcad004dd07ca01ea595f4c955a9381ffbb0ee
Anya Tchernishov <ania.cherni@gmail.com>
2014-02-07 10:47:26 +0200

    Add approve_exploit.

--- src/pantesting/db_access.py
+++ src/pantesting/db_access.py
@@ -1,6 +1,7 @@
 import json
 from flask import Blueprint, jsonify, request
 from pantesting.db.api import PanetesterApi
+from pantesting.db.orm import Exploit
 
 __author__ = 'newt'
 
@@ -48,9 +49,13 @@ def add_bounty():
     api.commit()
     return bounty
 
+
 @db_access.route("/approve_exploit/<exploit_id>", methods=["PUT"])
-def approve_exploit():
-    pass
+def approve_exploit(exploit_id):
+    exploit = api.get_exploits(id=exploit_id)[0]
+    exploit.status = Exploit.CONFIRMED
+    api.commit()
+    return "approved"
 
 
 @db_access.route('/example', methods=['GET', 'POST'])
